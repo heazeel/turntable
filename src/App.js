3002a23557f7786e5a3e3ed79a851d8da27f41b5
ranqiu <ranqiu.he@tuya.com>
2022-12-06 20:12:15 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useRef, useEffect } from 'react';
+import { LuckyWheel } from '@lucky-canvas/react';
+import { Modal } from 'antd';
+import Form from './form';
+
+const color1 = '#e9e8fe';
+const color2 = '#b8c5f2';
 
 function App() {
+  const [blocks] = useState([{ padding: '10px', background: '#869cfa' }]);
+  const [prizes, setPrizes] = useState();
+  const [buttons] = useState([
+    { radius: '35%', background: '#617df2' },
+    { radius: '30%', background: '#afc8ff' },
+    {
+      radius: '25%',
+      background: '#869cfa',
+      pointer: true,
+      fonts: [{ text: '吃什么', top: '-10px', fontColor: 'yellow' }]
+    }
+  ]);
+  const myLucky = useRef();
+
+  useEffect(() => {
+    if (!localStorage.getItem('prizes')) {
+      let initPrizes = [
+        { background: color1, fonts: [{ text: '海底捞', top: '50px' }], range: 1 },
+        { background: color2, fonts: [{ text: '螺蛳粉', top: '50px' }], range: 1 },
+        { background: color1, fonts: [{ text: '小馄饨', top: '50px' }], range: 1 },
+        { background: color2, fonts: [{ text: '薄饼', top: '50px' }], range: 1 },
+        { background: color1, fonts: [{ text: '麻辣烫', top: '50px' }], range: 1 },
+        { background: color2, fonts: [{ text: '汉堡', top: '50px' }], range: 1 }
+      ];
+      setPrizes(initPrizes);
+      localStorage.setItem('prizes', JSON.stringify(initPrizes));
+    } else {
+      try {
+        let initPrizes = JSON.parse(localStorage.getItem('prizes'));
+        setPrizes(initPrizes);
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="turntable_title">张总干饭小转盘</div>
+      <div className="turntable_wrap">
+        <LuckyWheel
+          ref={myLucky}
+          width="600px"
+          height="600px"
+          blocks={blocks}
+          prizes={prizes}
+          buttons={buttons}
+          onStart={() => {
+            // 点击抽奖按钮会触发star回调
+            myLucky.current.play();
+            setTimeout(() => {
+              const index = (Math.random() * 6) >> 0;
+              myLucky.current.stop(index);
+            }, 2500);
+          }}
+          onEnd={(prize) => {
+            // 抽奖结束会触发end回调
+            Modal.success({
+              title: `今天吃【${prize.fonts[0].text}】`
+            });
+          }}
+        />
+        <Form prizes={prizes} setPrizes={setPrizes} />
+      </div>
+    </>
   );
 }
 
---
+++ src/form/index.js
@@ -0,0 +1,85 @@
+import React, { useEffect } from 'react';
+import { MinusCircleOutlined, PlusOutlined } from '@ant-design/icons';
+import { Button, Form, Input, Space, message } from 'antd';
+
+import './index.css';
+
+const color1 = '#e9e8fe';
+const color2 = '#b8c5f2';
+
+const FormTable = (props) => {
+  const { prizes, setPrizes } = props;
+  const [form] = Form.useForm();
+
+  const onFinish = (values) => {
+    let res = values.foods.map((item, index) => {
+      return {
+        background: index % 2 ? color2 : color1,
+        fonts: [{ text: item.name, top: '50px' }],
+        range: item.range ? item.range : 1
+      };
+    });
+    setPrizes(res);
+    localStorage.setItem('prizes', JSON.stringify(res));
+    message.success('修改成功！');
+  };
+
+  useEffect(() => {
+    if (prizes) {
+      let values = prizes.map((item) => {
+        let name = item.fonts[0].text;
+        let range = item.range;
+        return { name, range };
+      });
+      form.setFieldValue('foods', values);
+    }
+  }, [prizes]);
+
+  return (
+    <div className="turntable_form">
+      <Form form={form} name="dynamic_form_complex" onFinish={onFinish} autoComplete="off">
+        <Form.List name="foods">
+          {(fields, { add, remove }) => (
+            <>
+              {fields.map(({ key, name, ...restField }) => (
+                <Space key={key} align="baseline">
+                  <Form.Item
+                    {...restField}
+                    label="菜名名称"
+                    name={[name, 'name']}
+                    rules={[{ required: true, message: '请填写名称' }]}
+                  >
+                    <Input placeholder="请输入名称" style={{ width: 260 }} />
+                  </Form.Item>
+
+                  <Form.Item
+                    {...restField}
+                    label="比例"
+                    name={[name, 'range']}
+                    rules={[{ required: true, message: '请填写比例' }]}
+                  >
+                    <Input placeholder="一位正整数" style={{ width: 150 }} />
+                  </Form.Item>
+                  <MinusCircleOutlined onClick={() => remove(name)} />
+                </Space>
+              ))}
+
+              <Form.Item>
+                <Button type="dashed" onClick={() => add()} block icon={<PlusOutlined />}>
+                  增加菜品
+                </Button>
+              </Form.Item>
+            </>
+          )}
+        </Form.List>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            提交
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
+
+export default FormTable;
